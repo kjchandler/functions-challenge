af20f275b0906ded2427158afb20ac4e234bb408
Kelly Chandler <chandlerkelly@hotmail.com>
2015-06-19 19:34:31 -0400

    function challenge

---
+++ Calc two numbers.js
@@ -0,0 +1,14 @@
+//calling the function myfunction and passing the two values 4 and 5
+//to the function to be calculated
+//the function myFunction receives the values 4 and 5 and are
+//stored in a and b. The function returns the value to the original
+//call and stored in the variable x and logged to the console.
+     
+function myFunction(a,b){   
+
+    return a * b;
+}
+var x = myFunction(4,5);
+console.log(x);
+
+
